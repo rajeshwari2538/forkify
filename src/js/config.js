eb243eeae1b728682e8ec4754c2b3b3cd619c0c2
rajeshwari2538 <rajeshwari2538@gmail.com>
2024-08-17 23:14:47 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a1e2e305-26b1-43a9-8231-7bf3832fa593';
+export const MODAL_CLOSE_SEC = 2.5;
